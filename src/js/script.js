2357d720389d5612638d87747e733369a66c9504
marcusmolchany <marcusmolchany@gmail.com>
2015-01-01 20:30:06 -0500

    Styled page, added js for flashcards

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,25 @@ var main = function() {
   var flashcards = flashcardGenerator;
   flashcards.getFlashcards();
 
-  console.log(flashcards);
+  $(".start").click(function() {
+    getNextQuestion(flashcards);
+    $(this).hide();
+  });
+
+  $(".flip").click(function() {
+    $(".answer").addClass("show");
+  });
+
+  $(".next").click(function() {
+    getNextQuestion(flashcards);
+    $(".answer").removeClass("show");
+  });
+};
+
+var getNextQuestion = function(flashcards) {
+  var flashcard = flashcards.getRandomFlashcard();
+  $(".question").text(flashcard.question);
+  $(".answer").text(flashcard.answer);
 };
 
 $(document).ready(main);
